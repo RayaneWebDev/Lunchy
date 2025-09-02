a34d97247ece2424a891b10244ed36b2cb6d0d16
RayaneTch <rayanewebdev7@gmail.com>
2025-09-02 22:51:18 +0200

    update backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try{
-        await mongoose.connect('mongodb://localhost:27017/Lunchy')
+        await mongoose.connect(process.env.MONGO_URI)
     }
     catch(e){
         console.log(e)
